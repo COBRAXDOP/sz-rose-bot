46ef5e604fcf4a1202c480ec45bb1b6c0463a13e
COBRAXDOP <85988755+COBRAXDOP@users.noreply.github.com>
2021-09-21 23:30:26 +0530

    Update __repo.py

--- DewmiBot/modules/__repo.py
+++ DewmiBot/modules/__repo.py
@@ -5,18 +5,18 @@ from pyrogram.types import *
 from DewmiBot.config import get_str_key
 from DewmiBot import pbot
 
-REPO_TEXT = "**A Powerful BOT to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 @supunmabot 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Resently\n╰──────────────\n\n»»» @szrosebot «««"
+REPO_TEXT = "**A Powerful BOT to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 @XD_LIF 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Resently\n╰──────────────\n\n»»» @AlizaProBot «««"
   
 BUTTONS = InlineKeyboardMarkup(
       [[
-        InlineKeyboardButton("Repository", url=f"https://github.com/youtubeslgeekshow/sz-rose-bot"),
-        InlineKeyboardButton("Video info ", url=f"https://www.youtube.com/channel/UCvYfJcTr8RY72dIapzMqFQA"),
+        InlineKeyboardButton("Repository", url=f"https://t.me/Xd_lif"),
+        InlineKeyboardButton("Video info ", url=f"https://t.me/L0VEXWORLD"),
       ],[
-        InlineKeyboardButton("𝑺𝒍 𝑩𝒐𝒕 𝒁𝒐𝒏𝒆 ", url="https://t.me/SL_bot_zone"),
-        InlineKeyboardButton("𝓢𝓛 𝓑𝓸𝓽 𝓒𝓱𝓪𝓽", url="https://t.me/slbotzone"),
+        InlineKeyboardButton("Support ", url="https://t.me/MISTY_SUPORTER"),
+        InlineKeyboardButton("Update", url="https://t.me/MISTY_SUPORT"),
       ],[
-        InlineKeyboardButton("rosebot update info", url="https://t.me/szroseupdates"),
-        InlineKeyboardButton("Developer", url="https://t.me/supunmabot"),
+        InlineKeyboardButton("Aliza update info", url="https://t.me/MISTY_SUPORT"),
+        InlineKeyboardButton("Developer", url="https://t.me/XD_LIF"),
       ]]
     )
   
